a195372f7daa398ac8c919e5b488fbb0ab6a8d00
Joseph Lai <jlai24142@gmail.com>
2024-02-28 10:30:39 -0600

    Updating the keybinding to pull up the Lazy plugin manager UI

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -3,11 +3,11 @@
 -- `<leader>F` - Run the `:Format` command to format code.
 vim.keymap.set("n", "<leader>F", "<cmd>Format<CR>")
 
+-- `<leader>L` - Run the `:Lazy` command to bring up the Lazy plugin manager UI.
+vim.keymap.set("n", "<leader>L", "<cmd>Lazy<CR>")
+
 -- `<leader>Z` - Run the `:ZenMode` command to toggle ZenMode.
 vim.keymap.set("n", "<leader>Z", "<cmd>ZenMode<CR>")
 
 -- `<C-t>` - Run the `:NERDTreeToggle` command to toggle the tree view pane.
 vim.keymap.set("n", "<C-t>", "<cmd>NERDTreeToggle<CR>")
-
--- `<C-L>` - Run the `:Lazy` command to bring up the Lazy plugin manager UI.
-vim.keymap.set("n", "<C-L>", "<cmd>Lazy<CR>")
