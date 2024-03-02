af3717e93ed2a11c862226d72e1dfab0da82e874
Joseph Lai <jlai24142@gmail.com>
2024-03-02 15:06:55 -0600

    Adding additional keybindings to create, delete, and rename nodes (files and directories) in nvim.tree

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -260,10 +260,16 @@ require("lazy").setup({
 			})
 
 			local nvim_tree_api = require("nvim-tree.api")
-			-- Open a node in the tree in a vertical split.
-			vim.keymap.set("n", "<leader>V", nvim_tree_api.node.open.vertical, {})
+			-- Create a new node in the tree (create a new file).
+			vim.keymap.set("n", "<leader>C", nvim_tree_api.fs.create, {})
+			-- Delete a node from the tree (delete a file).
+			vim.keymap.set("n", "<leader>D", nvim_tree_api.fs.remove, {})
+			-- Rename a node in the tree (rename file or directory).
+			vim.keymap.set("n", "<leader>R", nvim_tree_api.fs.rename, {})
 			-- Open a node in the tree in a horizontal split.
 			vim.keymap.set("n", "<leader>S", nvim_tree_api.node.open.horizontal, {})
+			-- Open a node in the tree in a vertical split.
+			vim.keymap.set("n", "<leader>V", nvim_tree_api.node.open.vertical, {})
 		end,
 		dependencies = {
 			-- `nvim-web-devicons` -  Lua fork of `vim-devicons` for Neovim.
