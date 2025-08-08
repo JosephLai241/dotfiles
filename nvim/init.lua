fa16a1027553da6531d88f006c42f7c617dc5be4
Joseph Lai <jlai24142@gmail.com>
2025-08-08 03:13:51 -0500

    Adding a function to enable auto-completion with the native LSP

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,12 +6,8 @@ require("vimcommands")
 -- Load all Neovim diagnostics settings.
 require("diagnostics")
 
--- Load all plugins.
-require("plugins")
-
 -- Load all custom keybindings.
 local keybindings = require("keybindings")
-
 -- Then call the `setup_lsp_keymaps()`function to override default keybindings.
 vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(ev)
@@ -36,3 +32,16 @@ vim.lsp.enable({
 	"taplo",
 	"tsserver",
 })
+
+-- Enable auto-completion.
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client:supports_method('textDocument/completion') then
+			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+		end
+	end,
+})
+
+-- Load all plugins.
+require("plugins")
