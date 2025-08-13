035d178d71b6156ae0af010c7b0650461f151581
Joseph Lai <jlai24142@gmail.com>
2025-08-13 18:54:48 -0500

    Removing a deprecated keybinding

--- nvim/lua/config/keybindings.lua
+++ nvim/lua/config/keybindings.lua
@@ -6,9 +6,6 @@ vim.keymap.set("n", "<leader>bdo", "<cmd>%bd|e#|bd#<CR>")
 -- `<leader>col` - Run the `:ColorizerToggle` command to toggle color previews.
 vim.keymap.set("n", "<leader>col", "<cmd>ColorizerToggle<CR>")
 
--- `<leader>F` - Run the `:Format` command to format code.
-vim.keymap.set("n", "<leader>F", "<cmd>Format<CR>")
-
 -- `<leader>L` - Run the `:Lazy` command to bring up the Lazy plugin manager UI.
 vim.keymap.set("n", "<leader>L", "<cmd>Lazy<CR>")
 
