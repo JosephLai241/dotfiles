184027857875739bb63b4ef872235a83faf21264
Joseph Lai <jlai24142@gmail.com>
2025-12-19 21:05:57 -0600

    Minor updates to echo calls

--- setup_scripts/setup_tmux.sh
+++ setup_scripts/setup_tmux.sh
@@ -3,15 +3,15 @@
 echo "=== Setting up Tmux ==="
 
 # Update system and install Tmux.
-echo "Installing Tmux..."
+echo "=== Installing Tmux..."
 sudo apt-get update && sudo apt-get install tmux
 
 # Install Tmux Plugin Manager.
-echo "Installing Tmux Plugin Manager..."
+echo "=== Installing Tmux Plugin Manager..."
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # Copy the `.tmux.conf` file from this repo to the user home directory.
-echo "Copying .tmux.conf to the home directory..."
+echo "=== Copying .tmux.conf to the home directory..."
 cp ../tmux/.tmux.conf $HOME
 
 echo "=== Finished running setup script for Tmux ==="
