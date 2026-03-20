f2dafd1fefa24d56dad8a70a42e2b7694577ddcd
Joseph Lai <jlai24142@gmail.com>
2026-03-19 21:44:29 -0500

    Adding NerdFont signs and number highlighting for diagnostics

--- nvim/lua/config/diagnostics.lua
+++ nvim/lua/config/diagnostics.lua
@@ -4,5 +4,19 @@
 -- See all options: https://neovim.io/doc/user/diagnostic.html#vim.diagnostic.Opts
 -- See blog post: https://gpanders.com/blog/whats-new-in-neovim-0-11/#diagnostics
 vim.diagnostic.config({
-	virtual_text = true
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "󰅚 ", -- nf-md-close_circle
+			[vim.diagnostic.severity.WARN] = "󰀪 ", -- nf-md-alert
+			[vim.diagnostic.severity.INFO] = "󰋽 ", -- nf-md-information
+			[vim.diagnostic.severity.HINT] = "󰌶 ", -- nf-md-lightbulb
+		},
+		-- Highlight the line number itself
+		numhl = {
+			[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+			[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+			[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+			[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+		},
+	},
 })
